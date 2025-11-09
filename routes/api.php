05242c1f6b73e8ac82c472a2f0090f50d5f0a0d9
Masjoel <masjoel@gmail.com>
2025-11-10 06:23:49 +0700

    update route register-app

--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::post('login', [AuthController::class, 'login']);
-Route::post('register', [AuthController::class, 'register']);
+Route::post('register-app', [AuthController::class, 'register']);
 Route::post('change-password', [AuthController::class, 'changepassword']);
 Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 Route::apiResource('products', ProductController::class)->middleware('auth:sanctum');
@@ -33,7 +33,7 @@ Route::apiResource('orders', OrderController::class)->middleware('auth:sanctum')
 Route::post('savedeviceid', [AuthController::class, 'savedeviceid'])->middleware('auth:sanctum');
 Route::get('prospect/{id}', [AuthController::class, 'prospect'])->middleware('auth:sanctum');
 Route::get('marketing', [AuthController::class, 'marketing']);
-// Route::post('/register', [RegistrationController::class, 'store']);
+Route::post('/register', [RegistrationController::class, 'store']);
 Route::post('/register-reseller', [RegistrationController::class, 'storeReseller']);
 Route::post('/data-reseller', [RegistrationController::class, 'dataReseller']);
 Route::post('/sync/categories', [CategoryController::class, 'sync']);
